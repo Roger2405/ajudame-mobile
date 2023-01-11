fffb354378a0d6c54d8ae9abc8cac5fa293f6ae1
Roger <rogerdr.rdr@gmail.com>
2023-01-11 19:57:44 -0300

    configuring stack navigation header

--- src/routes/appStack.routes.tsx
+++ src/routes/appStack.routes.tsx
@@ -1,6 +1,9 @@
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { Button } from "react-native";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
 import { AddProduct } from "../screens/AddProduct";
 import { AddSales } from "../screens/AddSales";
 import { Summary } from "../screens/Summary";
@@ -9,23 +12,39 @@ import { TabRoutes } from "./appTab.routes";
 
 export function AppRoutes() {
     const { Screen, Navigator } = createNativeStackNavigator();
+    const colorScheme = useColorScheme();
     return (
         <Navigator
             screenOptions={{
-                headerShown: false,
+                headerShown: true,
+                headerStyle: { backgroundColor: Colors.primary },
+                // headerRight: () => <Button title="teste" />,
+                headerBlurEffect: "dark",
+                // headerTitleStyle: { color: Colors[colorScheme].textContrast },
+                statusBarTranslucent: true,
+                headerTintColor: Colors[colorScheme].textContrast
             }}
         >
             <Screen
                 name='Root'
                 component={TabRoutes}
+                options={{
+                    headerShown: false,
+                }}
             />
             <Screen
                 name='AddSales'
                 component={AddSales}
+                options={{
+                    title: 'Adicionar Venda'
+                }}
             />
             <Screen
                 name='Summary'
                 component={Summary}
+                options={{
+                    title: 'Resumo da Venda'
+                }}
             />
             <Screen
                 name='AddProduct'
