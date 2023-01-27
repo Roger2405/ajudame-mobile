e617a5085d2dc0cfe6b8994937de7f21dcbd6a0e
Roger2405 <rogerdr.rdr@gmail.com>
2023-01-27 08:00:19 -0300

    Update orderProduct.ts

--- src/@types/orderProduct.ts
+++ src/@types/orderProduct.ts
@@ -2,15 +2,33 @@ export type OrderProductProps = {
     id_product: number
     price_product: number
     name_product: string
+    cost_product?: number
     count: number
+}
+
+export type LastSaleProductProps = {
+    count: number,
+    id_product: number,
+    id_sale: number,
+    time_sale: string,
+    name_product: string,
+    price_product: number,
+    type_product: string,
 }
 
 export type SaleProductProps = {
     count: number,
     id: number,
-    time: string,
-    date_sale: string,
     name_product: string,
     price_product: number,
     type_product: string,
 }
+/*
+    "id_sale": 102,
+    "id_product": 5,
+    "price_product": 7,
+    "count": 1,
+    "name_product": "Suco de Mimosa",
+    "type_product": "outros",
+    "time_sale": "17:59:24"
+*/
