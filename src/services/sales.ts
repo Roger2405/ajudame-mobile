e1bf52ba725bfdf3a56d52b205ce902c0fb84354
Roger2405 <rogerdr.rdr@gmail.com>
2023-01-27 07:37:43 -0300

    endpoints API atualizados

--- src/services/sales.ts
+++ src/services/sales.ts
@@ -1,5 +1,5 @@
-import { AxiosPromise } from "axios";
-import { OrderProductProps, SaleProductProps } from "../@types/orderProduct";
+
+import { LastSaleProductProps, OrderProductProps, SaleProductProps } from "../@types/orderProduct";
 import { SalesResumeProps } from "../@types/sales";
 import api from "./api";
 import getUserID from "./getUserID";
@@ -16,7 +16,7 @@ export async function getRecentSales() {
     const formatedMonth = (month + 1).toString().padStart(2, '0');
 
     const formatedDate = `${fullYear}-${formatedMonth}-${formatedDay}`;
-    const response = await api.get(`/${ID_USER}/sales/${formatedDate}`)
+    const response = await api.get(`/${ID_USER}/sales/recent`)
     const salesOfDay: SaleProductProps[] = response.data;
 
     return salesOfDay;
@@ -35,15 +35,18 @@ export async function getSalesResume() {
         })
     return salesResume
 }
-export async function updateSalesOnDB(orderProducts: OrderProductProps[]) {
+export async function updateSalesOnDB(orderProducts: OrderProductProps[], discountStock: boolean) {
     const ID_USER = await getUserID();
 
     const strOrderProducts = JSON.stringify(Array.from(orderProducts));
+    discountStock = true;
 
     return new Promise((resolve, reject) => {
         // setTimeout(() => {
-        api.post(`${ID_USER}/sales/register`, {
-            orderProducts: strOrderProducts
+        console.log(strOrderProducts);
+        api.post(`${ID_USER}/sales/last`, {
+            orderProducts: strOrderProducts,
+            discountStock
         })
             .then(res => {
                 if (res.data.success)
@@ -53,7 +56,7 @@ export async function updateSalesOnDB(orderProducts: OrderProductProps[]) {
                 }
             })
             .catch(err => {
-                reject(err)
+                reject('Ocorreu um erro no servidor!')
             })
         // }, 2000)
     })
@@ -62,16 +65,16 @@ export async function updateSalesOnDB(orderProducts: OrderProductProps[]) {
 export async function getLastSale() {
     const ID_USER = await getUserID();
     const response = await api.get(`/${ID_USER}/sales/last`)
-    const lastSale: SaleProductProps[] = response.data;
+    const lastSale: LastSaleProductProps[] = response.data;
 
     return lastSale;
 }
 
-export async function deleteSale(dateTimeSale: string) {
+export async function deleteLastSale() {
     const ID_USER = await getUserID();
 
     return new Promise((resolve, reject) => {
-        api.delete(`/${ID_USER}/sales/delete/${dateTimeSale}`)
+        api.delete(`/${ID_USER}/sales/last/`)
             .then(res => {
                 if (res.data.success) {
                     resolve(res.data.msg);
