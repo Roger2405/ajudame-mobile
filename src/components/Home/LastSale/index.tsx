3ec63a5d1c96fc412d6a602e723c63f892aa7f5b
Roger2405 <rogerdr.rdr@gmail.com>
2023-01-27 08:11:18 -0300

    last sale component

--- src/components/Home/LastSale/index.tsx
+++ src/components/Home/LastSale/index.tsx
@@ -1,19 +1,19 @@
 
+import { Feather } from '@expo/vector-icons';
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import { SaleProductProps } from '../../../@types/orderProduct';
+import { LastSaleProductProps } from '../../../@types/orderProduct';
 import Colors from '../../../constants/Colors';
 import { useRecentSales } from '../../../contexts/sales';
-import { deleteSale } from '../../../services/sales';
+import useColorScheme from '../../../hooks/useColorScheme';
 import { DeleteButton } from '../../common/Buttons';
 import ConfirmationModal from '../../common/ConfirmationModal';
 import { FeedbackMessage } from '../../common/FeedbackMessage';
-import { SalesListItem } from '../../SalesList';
 
 import { styles } from './styles';
 
 interface Props {
-    data: SaleProductProps[]
+    data: LastSaleProductProps[]
     handleDeleteSale: () => void
 }
 
@@ -21,39 +21,59 @@ export function LastSale({ data, handleDeleteSale }: Props) {
     const [showConfirmationModal, setShowConfirmationModal] = useState(false);
     const [feedbackMessage, setFeedbackMessage] = useState<{ type: 'error' | 'info', msg: string }>({} as { type: 'error' | 'info', msg: string });
 
-    const { updateRecentSalesInContext } = useRecentSales();
-
+    
     return (
-        <>
-            <View style={styles.container} >
+        <View style={styles.container} >
+            {
+                feedbackMessage.msg &&
+                <FeedbackMessage feedbackMessage={feedbackMessage} setFeedbackMessage={setFeedbackMessage} />
+            }
+            <View style={styles.header}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: Colors.gray }}>
+                    Última venda:
+                </Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', marginLeft: 'auto' }}>
+                    <Feather name='clock' size={16} color={Colors.lightGray} />
+                    <Text style={[styles.time, { color: Colors.lightGray }]}>{(data[0].time_sale)}</Text>
+                </View>
+                <DeleteButton onPress={() => {
+                    setShowConfirmationModal(true)
+                }} />
+            </View>
+            <View style={{ paddingTop: 4 }}>
                 {
-                    feedbackMessage.msg &&
-                    <FeedbackMessage feedbackMessage={feedbackMessage} setFeedbackMessage={setFeedbackMessage} />
+                    data.map(sale => {
+                        return <LastSalesListItem key={sale.id_product} item={sale} />
+                    })
                 }
-                <View style={styles.header}>
-                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: Colors.gray }}>
-                        Última venda:
-                    </Text>
-                    <Text style={styles.time}>{(data[0]?.time)}</Text>
-                    <DeleteButton onPress={() => {
-                        setShowConfirmationModal(true)
-                    }} />
-                </View>
-                <View style={{ paddingTop: 4 }}>
-                    {
-                        data.map(sale => {
-                            return <SalesListItem key={sale.id} item={sale} />
-                        })
-                    }
-                </View>
-            </View >
-
+            </View>
             <ConfirmationModal showConfirmationModal={showConfirmationModal} setShowConfirmationModal={setShowConfirmationModal} onConfirm={() => {
                 handleDeleteSale()
                 setShowConfirmationModal(false)
             }} />
+        </View >
 
 
-        </>
+    );
+}
+
+interface ItemProps {
+    item: LastSaleProductProps
+}
+
+export function LastSalesListItem({ item }: ItemProps) {
+    const colorScheme = useColorScheme();
+    return (
+        <View style={[{ backgroundColor: Colors[colorScheme].itemColor }, styles.item]}>
+            <Text
+                style={[styles.itemName, styles.text, { flexGrow: 1, color: Colors[colorScheme].text }]}
+            >{item.name_product}</Text>
+            <Text
+                style={[styles.text, styles.itemPrice, { color: Colors[colorScheme].text }]}
+            >R$ {item.price_product.toFixed(2)}</Text>
+            <Text
+                style={[styles.itemCount, { backgroundColor: Colors.bgSmooth, color: Colors[colorScheme].itemColor }]}
+            >x {item.count}</Text>
+        </View>
     );
 }
--- src/components/Home/LastSale/styles.ts
+++ src/components/Home/LastSale/styles.ts
@@ -10,6 +10,40 @@ export const styles = StyleSheet.create({
         height: 32, paddingHorizontal: 4, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'
     },
     time: {
-        marginLeft: 'auto', marginRight: 16, fontWeight: '300', fontSize: 16
+        marginLeft: 4, marginRight: 16, fontSize: 16, fontWeight: '900'
+    },
+    item: {
+        flexDirection: 'row',
+        marginTop: 4,
+        padding: 8,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderRadius: 8,
+        maxWidth: '100%',
+
+        borderBottomWidth: 1,
+        borderColor: Colors.lightGray
+
+        // elevation: 4,
+
+    },
+    itemName: {
+        flexBasis: '50%',
+    },
+    itemCount: {
+        padding: 4,
+        width: 50,
+        borderRadius: 16,
+        textAlign: 'center',
+        fontWeight: '900',
+    },
+    itemPrice: {
+        width: 70,
+        marginHorizontal: 8,
+        textAlign: 'right',
+    },
+    text: {
+        textTransform: 'uppercase',
+        fontSize: 16,
     }
 })
